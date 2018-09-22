3a656ee222b0df4f2c729482425983e692b2d9ff
Patryk Wilk <piess105@gmail.com>
2018-09-22 13:19:57 +0200

    Dodaj pliki projektów.

---
+++ Tasker.Service/Infrastructure/Installer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Facilities.Logging;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Services.Logging.Log4netIntegration;
+using Castle.Windsor;
+
+namespace Tasker.Service.Infrastructure
+{
+    public class Installer : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.AddFacility<LoggingFacility>(f => f.LogUsing<Log4netFactory>());
+         //   container.AddFacility<PersistenceFacility>();
+
+            container.Register(
+
+                Component
+                    .For<MyService>()
+                    .LifeStyle.Transient
+
+            );
+        }
+    }
+}
